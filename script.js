2aa2abe4be3e91649710d409bf1be881ea64647b
maximbrail <maxabobus953@gmail.com>
2024-08-17 16:51:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+
+// const color = prompt("Choose color: red, blue or green?")
+
+
+
+// const box = document.querySelector(".box")
+
+// console.log(box);
+
+
+// if (color === 'red'){
+//     box.classList.add("red")
+// } else if (color === 'blue') {
+//     box.classList.add('blue')
+// } else if (color === 'green') {
+//     box.classList.add('green')
+// } else {
+//     alert("Вы ввели неверный текст!")
+// }
+
+
+
+
+
+// const title = document.querySelector("h1")
+
+// const newtext = prompt("Введите новый текст для заголовка: ЖОПА")
+
+// if (newtext){
+//     title.textContent = newtext
+// } else {
+//     alert("Вы ничего не ввели")
+// }
+
+
+
+
+// const name = document.querySelector('.box h1')
+
+// const surname = document.querySelector('.box h2')
+
+// const age = document.querySelector('.box h3')
+
+// const img = document.querySelector('.box img')
+
+
+
+// const fName = prompt("Name")
+
+// const sName = prompt("Surname")
+
+// const ageYour = prompt("Age")
+
+// const link = prompt("Link on your img")
+
+
+// name.textContent = fName
+
+// surname.textContent = sName
+
+// age.textContent = ageYour
+
+// img.src = link
